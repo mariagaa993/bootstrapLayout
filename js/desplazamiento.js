b344a63e40cbfff683728053d7be6abb1ae9bd4a
Maria Alarcon <mariagaa993@gmail.com>
2020-07-02 01:40:10 -0400

    first commit

---
+++ js/desplazamiento.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    // Desplazamiento de todos los enlaces en la barra de navegación + enlace de pie de página
+    $("footer a[href='#page-top']").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+  
+        // Usando animate para agregar desplazamiento de la página
+        // Especifica la cantidad de milisegundos que se tarde en desplazarse.
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+    
+    $(window).scroll(function() {
+      $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+  
+        var winTop = $(window).scrollTop();
+          if (pos < winTop + 600) {
+            $(this).addClass("slide");
+          }
+      });
+    });
+  })
